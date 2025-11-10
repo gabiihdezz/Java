cff1e5d3cb3386de684c46699f5dead9436685d1
Gabriel Hernandez <151533194+gabiihdezz@users.noreply.github.com>
2025-11-10 19:41:07 +0100

    Hibernate

---
+++ ProyectoInicioHibernate/src/main/java/proyectoInicioHibernate/entidades/Profesor.java
@@ -0,0 +1,45 @@
+package proyectoInicioHibernate.entidades;
+
+import javax.persistence.*;
+
+public class Profesor {
+	@Id
+	@Column(name="cod_profesor")
+	private String cod_profesor;
+	@Column(name="nombre_profesor")
+	private String nombre_profesor;
+	@Column(name="ciudad")
+	private String ciudad;
+	public String getCod_profesor() {
+		return cod_profesor;
+	}
+	public void setCod_profesor(String cod_profesor) {
+		this.cod_profesor = cod_profesor;
+	}
+	public String getNombre_profesor() {
+		return nombre_profesor;
+	}
+	public void setNombre_profesor(String nombre_profesor) {
+		this.nombre_profesor = nombre_profesor;
+	}
+	public String getCiudad() {
+		return ciudad;
+	}
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+	public Profesor(String cod_profesor, String nombre_profesor, String ciudad) {
+		super();
+		this.cod_profesor = cod_profesor;
+		this.nombre_profesor = nombre_profesor;
+		this.ciudad = ciudad;
+	}
+	
+	@Override
+	public String toString() {
+		return "Profesor [cod_profesor=" + cod_profesor + ", nombre_profesor=" + nombre_profesor + ", ciudad=" + ciudad
+				+ "]";
+	}
+	
+
+}
